eee22a7b1230b119d1a72cd45aa653bf048e55a8
David A. Adams <david.adams97@gmail.com>
2021-10-07 19:19:36 -0300

    feat(Header.tsx): add basic header

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './Header.css';
+
+export class Header extends React.Component {
+    render(): React.ReactElement {
+        return (
+            <div className="header">
+
+            </div>
+        )
+    }
+}
