7837ee0870a14656c0a7e9d7bde2ef83007a3029
David A. Adams <david.adams97@gmail.com>
2021-10-07 22:35:46 -0300

    refactor(Header): remove redundant files

--- src/components/header/Header.tsx
+++
@@ -1,8 +0,0 @@
-import React, { Component } from "react";
-import "./Header.css";
-
-export class Header extends Component {
-  render(): React.ReactElement {
-    return <div className="header"/>;
-  }
-}
