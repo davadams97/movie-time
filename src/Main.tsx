96473a66fb87eb44278076c7c95eb3a96681a139
David A. Adams <david.adams97@gmail.com>
2021-10-09 14:26:39 -0300

    feat(Main): add callbacks for header title change

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,39 +1,60 @@
 import './Main.css';
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Link, Route, Switch, Redirect } from 'react-router-dom';
 import { MovieList } from './pages/movie-list/MovieList';
 import { MovieInformation } from './pages/movie-information/MovieInformation';
 import { FaArrowLeft } from 'react-icons/fa';
 
-export class Main extends Component {
+export class Main extends Component<any, { pageName: string }> {
+    constructor(props: any) {
+        super(props);
+        this.setPageName = this.setPageName.bind(this);
+        this.state = { pageName: '' };
+    }
+
+    setPageName(name: string): void {
+        this.setState({ pageName: name });
+    }
+
+    determineTitle(): React.ReactElement | null {
+        const { pageName } = this.state;
+
+        // Since we want to conditionally render the back button, we capture the location to determine the page
+        if (window.location.pathname.split('/')[1] === 'movie-list') {
+            return (
+                <div className="navbar">
+                    <span className="header-title">{pageName}</span>
+                </div>
+            );
+        } else {
+            return (
+                <div className="navbar">
+                    <Link to="/movie-list">
+                        <FaArrowLeft className="navigation-icon" />
+                    </Link>
+                    <span className="header-title">{pageName}</span>
+                </div>
+            );
+        }
+    }
+
     render(): React.ReactElement {
         return (
             <div>
                 <Router>
-                    <div>
-                        <ul>
-                            <li>
-                                <Link to="/movie-list">
-                                    <FaArrowLeft />
-                                </Link>
-                            </li>
-                            <li>
-                                <Link to="/movie-information">
-                                    Movie Information
-                                </Link>
-                            </li>
-                        </ul>
-                    </div>
+                    {this.determineTitle()}
 
                     <Switch>
-                        <Route path="/movie-list">
-                            <MovieList />
-                        </Route>
-                        <Route path="/movie-information">
-                            <MovieInformation />
-                        </Route>
+                        <Route
+                            path="/movie-list"
+                            render={(props) => <MovieList {...props} callback={this.setPageName} />}
+                        />
+                        <Route
+                            path="/movie-information/:id"
+                            render={(props) => <MovieInformation {...props} callback={this.setPageName} />}
+                        />
                         <Route path="/">
-                            <MovieList />
+                            <Redirect to="/movie-list" />
                         </Route>
                     </Switch>
                 </Router>
