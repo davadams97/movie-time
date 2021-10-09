77ac94c84198ff1f65da4e7fbc50248ea1e84f5e
David Alexander Adams <35355999+davadams97@users.noreply.github.com>
2021-10-09 16:49:44 -0300

    Refactor/cleanup movie list page (#6)
    * feat(MovieList.tsx): cleanup fetch calls
    
    * feat(MovieList.css): add padding to grid
    
    * feat(MovieList.types): remove error properties

--- src/pages/movie-list/MovieList.tsx
+++ src/pages/movie-list/MovieList.tsx
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
 import { IState, Result, RootResult } from './MovieList.types';
 import './MovieList.css';
+import { handle } from '../../utility/handleError';
 
 export class MovieList extends Component<any, IState> {
     constructor(props: any) {
         super(props);
         this.state = {
-            error: null,
-            isLoaded: false,
             items: [],
         };
     }
@@ -16,28 +15,38 @@ export class MovieList extends Component<any, IState> {
      * Function creates "img" elements needed to display movies
      */
     createImageElements() {
-        const images = this.state.items;
+        const { items: images } = this.state;
         return images.map((image: Result) => (
-            <a href="">
-                <img
-                    src={`https://image.tmdb.org/t/p/w185/${image.poster_path}`}
-                    alt={`${image.title}`}
-                />
+            <a key={image.id} href={`/movie-information/${image.id}`}>
+                <img src={`https://image.tmdb.org/t/p/w154/${image.poster_path}`} alt={`${image.title}`} />
             </a>
         ));
     }
 
-    componentDidMount(): void {
-        fetch(
-            `http://api.themoviedb.org/3/movie/popular?api_key=${process.env.REACT_APP_API_KEY}`
-        )
-            .then((res: Response) => res.json())
-            .then((res: RootResult) =>
-                this.setState({ isLoaded: true, items: res.results })
-            );
+    /**
+     * Utility function load movie list
+     */
+    async loadMovieList(): Promise<RootResult> {
+        // TODO: Implementation of error handling can be improved. For now log to console.
+
+        const [movieList, err] = await handle(
+            fetch(`http://api.themoviedb.org/3/movie/popular?api_key=${process.env.REACT_APP_API_KEY}`)
+        );
+        if (err) console.error(err);
+
+        const [finalResponse, movieListError] = await handle(movieList.json());
+        if (movieListError) console.error(err);
+
+        return finalResponse;
+    }
+
+    async componentDidMount(): Promise<void> {
+        const finalResponse = await this.loadMovieList();
+        this.setState({ items: finalResponse.results });
+        this.props.callback('Popular Movies');
     }
 
     render(): React.ReactElement {
-        return <div>{this.createImageElements()}</div>;
+        return <div className="grid">{this.createImageElements()}</div>;
     }
 }
