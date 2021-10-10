918b329b9b8d3f14b38341630df118d92e1da9af
David A. Adams <david.adams97@gmail.com>
2021-10-10 02:31:13 -0300

    refactor(MovieInformation): use custom HTTP resolver

--- src/pages/movie-information/MovieInformation.tsx
+++ src/pages/movie-information/MovieInformation.tsx
@@ -1,19 +1,39 @@
 import React, { Component } from 'react';
 import { RootObject } from './MovieInformation.types';
-import { BsPlayCircle } from 'react-icons/all';
+import { BsPlayCircle } from 'react-icons/bs';
 import './MovieInformation.css';
+import { handle } from '../../utility/handleRequest';
+import { Description } from '../../components/description/Description';
 
-export class MovieInformation extends Component<any, { res: RootObject | null }> {
+export class MovieInformation extends Component<any, { res: Partial<RootObject> | null }> {
     constructor(props: any) {
         super(props);
         this.state = { res: null };
     }
 
+    /**
+     * Utility function load movie details
+     * @param id, movie ID
+     */
+    async loadMovie(id: number): Promise<RootObject> {
+        // TODO: Implementation of error handling can be improved. For now log to console.
+
+        const [movie, err] = await handle(
+            fetch(`http://api.themoviedb.org/3/movie/${id}?api_key=${process.env.REACT_APP_API_KEY}`)
+        );
+        if (err) console.error(err);
+
+        const [movieJson, movieListError] = await handle(movie.json());
+        if (movieListError) console.error(movieListError);
+
+        return movieJson;
+    }
+
     async componentDidMount(): Promise<void> {
         const { id } = this.props.match.params;
-        const res = await fetch(`http://api.themoviedb.org/3/movie/${id}?api_key=${process.env.REACT_APP_API_KEY}`);
-        const finalResponse = await res.json();
-        this.setState({ res: finalResponse });
+        const res = await this.loadMovie(id);
+
+        this.setState({ res: res });
         this.props.callback('Movie Details');
     }
 
